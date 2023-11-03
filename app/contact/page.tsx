d16e86a4a212e3a57cfe54e3defe6e5dbbeb3768
fh.prv <fh@fhprv.de>
2023-11-03 21:42:00 +0100

    v2.0.2-dev

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,18 @@
+import DoubleArrow from '@mui/icons-material/DoubleArrow';
+import CSS from '../../public/css/Contact.module.css';
+
 export default function Contact() {
     return (
-        <a>Hello Contact!</a>
+        <div className={CSS.contact}>
+            <div className={CSS.content}>
+                <div className={CSS.headline}>
+                    <a className={CSS.code}>04</a>. Contact
+                </div>
+                <div className={CSS.text}>
+                    If you have any questions, feel free to contact me. &lt;<a className={CSS.code}>br/</a>&gt;<br/>
+                    <a className={CSS.link} href="mailto:info@fhprv.de">info@fhprv.de</a> <DoubleArrow/>
+                </div>
+            </div>
+        </div>
     );
 }
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,5 +1,18 @@
+import DoubleArrow from '@mui/icons-material/DoubleArrow';
+import CSS from '../../public/css/News.module.css';
+
 export default function News() {
     return (
-        <a>Hello News!</a>
+        <div className={CSS.news}>
+            <div className={CSS.content}>
+                <div className={CSS.headline}>
+                    <a className={CSS.code}>404</a>. News
+                </div>
+                <div className={CSS.text}>
+                    This page is still in work. Have a look here: &lt;<a className={CSS.code}>br/</a>&gt;<br/>
+                    <a className={CSS.link} href="/about-me">About Me</a> <DoubleArrow/>
+                </div>
+            </div>
+        </div>
     );
 }
